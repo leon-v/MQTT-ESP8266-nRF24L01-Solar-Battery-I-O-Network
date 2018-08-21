8a11aebee9d17372f71a28710d5d382152ca4305
leon-v <leon@valkenb.org>
2018-08-21 22:01:59 +1200

    Getting httpd with custom files

--- Router/Firmware/src/components/component.mk
+++ Router/Firmware/src/components/component.mk
@@ -1,6 +1,8 @@
 
 COMPONENT_SRCDIRS += folder1 folder2\
 	../../ESP8266_RTOS_SDK/components/lwip/lwip/src/apps/httpd\
-	httpd_custom
+	httpd_custom\
+	httpd_custom/makefsdata
 
-COMPONENT_ADD_INCLUDEDIRS += httpd_custom
+COMPONENT_ADD_INCLUDEDIRS += httpd_custom\
+	httpd_custom/makefsdata
